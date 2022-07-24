05440c992c40ee1e8c7450f703dd4872f51653ba
Joana <99611541+JoneJoana@users.noreply.github.com>
2022-07-24 18:14:12 +0200

    arreglo duplicidades PK y FK
    una tabla con una PK compuesta, al ser FK en otra tabla, se le tiene que hacer referencia en conjunto , es decir a la FK (xx , xxx) de la tablaXXX.

--- UD12/ex05.sql
+++ UD12/ex05.sql
@@ -2,54 +2,48 @@ CREATE DATABASE ex5;
 USE ex5;
 
 CREATE TABLE TipoPiezas(
-	tipo varchar(5) PRIMARY KEY,
+    tipo varchar(5) PRIMARY KEY,
     descripcion varchar(100)
 );
 
 CREATE TABLE Piezas(
-	modelo int PRIMARY KEY,
+    modelo int,    
+    tipo varchar(5),   
     descripcion varchar(100),
     precio double,
-    tipo varchar(5),
+    PRIMARY KEY(modelo, tipo),
     CONSTRAINT FK_TipoPiezas FOREIGN KEY (tipo) REFERENCES TipoPiezas(tipo)
 );
 
 CREATE TABLE Almacenes(
 	idAlmacen int PRIMARY KEY,
-    descripcion varchar(100),
-    direccion varchar(100)
+	descripcion varchar(100),
+	direccion varchar(100)
 );
 
 CREATE TABLE Estanterias(
-	idEstanteria varchar(10),
+    idEstanteria varchar(10),
     idAlmacen int,
-    CONSTRAINT FK_EstanteriasAlmacenes FOREIGN KEY (idAlmacen) REFERENCES Almacenes(idAlmacen),
-    PRIMARY KEY(idEstanteria,idAlmacen)
+    PRIMARY KEY(idAlmacen,idEstanteria),
+    CONSTRAINT FK_EstanteriasAlmacenes FOREIGN KEY (idAlmacen) REFERENCES Almacenes(idAlmacen)
 );
 
 CREATE TABLE Almacena(
-	idAlmacen int,
-    idEstanteria varchar(10),
-    tipo varchar(5),
-    modelo int,
+    idAlmacen int, #pk compuesta de estanteria
+    idEstanteria varchar(10), #pk compuesta de estanteria
+    tipo varchar(5), #pk compuesta de pieza
+    modelo int, #pk compuesta de pieza
     cantidad int,
-    CONSTRAINT FK_AlmacenaAlmacen FOREIGN KEY (idAlmacen) REFERENCES Almacenes(idAlmacen),
-    CONSTRAINT FK_AlmacenaEstanteria FOREIGN KEY (idEstanteria,idAlmacen) REFERENCES Estanterias(idEstanteria,idAlmacen),
-    CONSTRAINT FK_AlmacenaTipo FOREIGN KEY (tipo) REFERENCES Piezas(tipo),
-    CONSTRAINT FK_AlmacenaModelo FOREIGN KEY (modelo) REFERENCES Piezas(modelo),
-	PRIMARY KEY(idAlmacen, idEstanteria, tipo, modelo)
+    PRIMARY KEY(idAlmacen,idEstanteria,tipo,modelo),
+    CONSTRAINT FK_AlmacenaEstanteria FOREIGN KEY (idAlmacen,idEstanteria) REFERENCES Estanterias(idAlmacen,idEstanteria),
+    CONSTRAINT FK_AlmacenaModelo FOREIGN KEY (modelo,tipo) REFERENCES Piezas(modelo,tipo)
 );
 
 CREATE TABLE Contiene(
-	id int AUTO_INCREMENT PRIMARY KEY,
-    tipo varchar(100),
-    modelo int,
+    tipo varchar(5), # pk compuesta de tabla Piezas
+    modelo int, # pk compuesta de tabla Piezas
     tipoCompuesto varchar(5),
-    modeloCompuesto int,
-    CONSTRAINT FK_ContieneTipo FOREIGN KEY (tipo) REFERENCES Piezas(tipo),
-    CONSTRAINT FK_ContieneModelo FOREIGN KEY (modelo) REFERENCES Piezas(modelo),
-    CONSTRAINT FK_ContieneTipoCompuesto FOREIGN KEY (tipo) REFERENCES Piezas(tipo),
-    CONSTRAINT FK_ContieneModeloCompuesto FOREIGN KEY (modelo) REFERENCES Piezas(modelo)
+    modeloCompuesto int,    
+    CONSTRAINT FK_ContieneModelo FOREIGN KEY (modelo,tipo) REFERENCES Piezas(modelo,tipo),    
+    CONSTRAINT FK_ContieneModeloCompuesto FOREIGN KEY (modelo,tipo) REFERENCES Piezas(modelo,tipo)
 );
-
-
